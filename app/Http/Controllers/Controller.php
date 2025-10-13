853ffd48c1f97e48957e3a24cc0399ceaf99842a
armazade <armantje2001@gmail.com>
2025-10-13 15:56:45 +0200

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+
+abstract class Controller
+{
+    public function index()
+    {
+        return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
