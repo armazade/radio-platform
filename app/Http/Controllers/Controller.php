36a45c631942a9d8bd916908ac89d429d3d53d0c
armazade <armantje2001@gmail.com>
2025-10-13 17:15:48 +0200

    Remove unnecessary index method from base Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,17 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Inertia\Inertia;
-
 abstract class Controller
 {
-    public function index()
-    {
-        return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-        ]);
-    }
+    //
 }
