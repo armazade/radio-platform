4092f15c523e23f82616df8febd0bb1236ef5482
armazade <armantje2001@gmail.com>
2025-10-15 15:48:43 +0200

    feat: add genre enums and selector component

--- domain/Episode/Enums/Genre.php
+++ domain/Episode/Enums/Genre.php
@@ -40,20 +40,4 @@ enum Genre: string
     {
         return array_column(self::cases(), 'value');
     }
-
-    /**
-     * Get color for badge display
-     */
-    public function color(): string
-    {
-        return match($this) {
-            self::AMBIENT, self::EXPERIMENTAL => 'bg-purple-100 text-purple-800',
-            self::JUNGLE, self::DRUM_AND_BASS => 'bg-green-100 text-green-800',
-            self::TECHNO, self::HOUSE => 'bg-blue-100 text-blue-800',
-            self::JAZZ, self::SOUL, self::FUNK => 'bg-yellow-100 text-yellow-800',
-            self::HIP_HOP, self::RNB => 'bg-red-100 text-red-800',
-            self::AFROBEAT, self::LATIN => 'bg-orange-100 text-orange-800',
-            default => 'bg-gray-100 text-gray-800',
-        };
-    }
 }
