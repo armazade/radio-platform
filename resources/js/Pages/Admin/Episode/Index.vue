74a98973396646fff5b6c2d522f57e077d1e1e66
armazade <armantje2001@gmail.com>
2025-10-19 00:00:47 +0200

    feat: add auth in episode index wip also in episode create

--- resources/js/Pages/Admin/Episode/Index.vue
+++ resources/js/Pages/Admin/Episode/Index.vue
@@ -1,40 +1,42 @@
 <template>
     <Head title="Episodes" />
+    <AuthenticatedLayout>
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <div v-if="$page.props.flash.success" class="mb-4 text-green-600">
+                            {{ $page.props.flash.success }}
+                        </div>
+                        <h1 class="text-2xl font-bold mb-4">Episodes</h1>
 
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div v-if="$page.props.flash.success" class="mb-4 text-green-600">
-                        {{ $page.props.flash.success }}
-                    </div>
-                    <h1 class="text-2xl font-bold mb-4">Episodes</h1>
-
-                    <ul class="space-y-4">
-                        <li
-                            v-for="episode in episodes"
-                            :key="episode.id"
-                            class="p-4 border rounded shadow"
-                        >
-                            <Link
-                                :href="route('admin.episodes.edit', episode.id)"
-                                class="block"
+                        <ul class="space-y-4">
+                            <li
+                                v-for="episode in episodes"
+                                :key="episode.id"
+                                class="p-4 border rounded shadow"
                             >
-                                <h2 >
-                                    {{ episode.title }}
-                                </h2>
-                                <p class="text-gray-700">{{ episode.description }}</p>
-                            </Link>
-                        </li>
-                    </ul>
+                                <Link
+                                    :href="route('admin.episodes.edit', episode.id)"
+                                    class="block"
+                                >
+                                    <h2 >
+                                        {{ episode.title }}
+                                    </h2>
+                                    <p class="text-gray-700">{{ episode.description }}</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
 
 <script setup>
 import { Head, Link } from '@inertiajs/vue3'
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 defineProps({
     episodes: Array
