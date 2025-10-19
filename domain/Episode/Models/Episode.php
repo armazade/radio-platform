7d139349e0e05b6894d32fed57731ec7be545f3f
armazade <armantje2001@gmail.com>
2025-10-19 11:54:06 +0200

    feat: add audio to model

--- domain/Episode/Models/Episode.php
+++ domain/Episode/Models/Episode.php
@@ -29,6 +29,7 @@ class Episode extends Model implements HasMedia
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('images')->singleFile();
+        $this->addMediaCollection('audio')->singleFile();
     }
 
     public function registerMediaConversions(Media $media = null): void
@@ -37,6 +38,42 @@ class Episode extends Model implements HasMedia
             ->width(200)
             ->height(200)
             ->sharpen(10);
+    }
+
+    /**
+     * Get the audio file URL
+     */
+    public function getAudioUrl(): ?string
+    {
+        return $this->getFirstMediaUrl('audio');
+    }
+
+    /**
+     * Get audio file metadata
+     */
+    public function getAudioMetadata(): ?array
+    {
+        $audio = $this->getFirstMedia('audio');
+
+        if (!$audio) {
+            return null;
+        }
+
+        return [
+            'url' => $audio->getUrl(),
+            'name' => $audio->file_name,
+            'size' => $audio->human_readable_size,
+            'mime_type' => $audio->mime_type,
+            'duration' => $audio->getCustomProperty('duration'), // Can be set when uploading
+        ];
+    }
+
+    /**
+     * Check if episode has audio
+     */
+    public function hasAudio(): bool
+    {
+        return $this->hasMedia('audio');
     }
 
     /**
