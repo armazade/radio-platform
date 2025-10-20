3a32bbd22736b5136ef3b571f5f8eeeaaa7bc0dc
armazade <armantje2001@gmail.com>
2025-10-20 15:17:33 +0200

    chore add more in seeder and genres

--- database/seeders/EpisodeSeeder.php
+++ database/seeders/EpisodeSeeder.php
@@ -57,5 +57,29 @@ class EpisodeSeeder extends Seeder
         ]);
         $episode6->setGenres([Genre::JAZZ, Genre::HOUSE]);
         $episode6->save();
+
+        $episode7 = Episode::create([
+            'title' => 'Tehrangeles',
+            'description' => 'A fusion of Persian traditional music with modern electronic beats.',
+            'date' => now()->subDays(9),
+        ]);
+        $episode7->setGenres([Genre::TRADITIONAL, Genre::EXPERIMENTAL, Genre::ORIENTAL]);
+        $episode7->save();
+
+        $episode8 = Episode::create([
+            'title' => 'Rupture w/ Mantra',
+            'description' => 'Sounds from the grass roots of jungle and drum & bass with London scene heroes Rupture ',
+            'date' => now()->subDays(9),
+        ]);
+        $episode8->setGenres([Genre::DRUM_AND_BASS, Genre::JUNGLE, Genre::AMBIENT]);
+        $episode8->save();
+
+        $episode9 = Episode::create([
+            'title' => 'Lu Yang',
+            'description' => 'Buddhist traditional music fused with contemporary electronic sounds by Chinese artist Lu Yang.',
+            'date' => now()->subDays(9),
+        ]);
+        $episode9->setGenres([Genre::ORIENTAL, Genre::WORLD, Genre::AMBIENT]);
+        $episode9->save();
     }
 }
