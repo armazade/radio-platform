efa7deec2ec37a24f35a3b4d8e3879255eb6dc32
armazade <armantje2001@gmail.com>
2025-10-21 21:23:59 +0200

    feat: improve create form with new inputs

--- resources/js/Pages/Admin/Episode/Create.vue
+++ resources/js/Pages/Admin/Episode/Create.vue
@@ -1,16 +1,17 @@
 <script setup>
-import GuestLayout from '@/Layouts/GuestLayout.vue'
 import InputError from '@/Components/InputError.vue'
 import InputLabel from '@/Components/InputLabel.vue'
 import PrimaryButton from '@/Components/PrimaryButton.vue'
 import TextInput from '@/Components/TextInput.vue'
 import GenreSelector from '@/Components/GenreSelector.vue'
 import {Head, useForm} from '@inertiajs/vue3'
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
 const form = useForm({
     title: '',
     description: '',
     date: '',
+    location: '',
     genres: [],
     image: null,
 })
@@ -23,10 +24,10 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Create Episode" />
+    <AuthenticatedLayout>
+        <Head title="Edit Episode" />
 
-        <form @submit.prevent="submit" class="max-w-xl mx-auto space-y-6 mt-10">
+        <form @submit.prevent="submit" class="max-w-4xl mx-auto space-y-6 mt-10">
             <div>
                 <InputLabel for="title" value="Episode Title" />
 
@@ -46,28 +47,43 @@ const submit = () => {
             <div>
                 <InputLabel for="description" value="Description" />
 
-                <textarea
+                <TextInput
                     id="description"
                     v-model="form.description"
-                    class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                    rows="4"
-                ></textarea>
+                    class="mt-1 block w-full"
+                ></TextInput>
 
                 <InputError class="mt-2" :message="form.errors.description" />
             </div>
 
-            <div>
-                <InputLabel for="date" value="Date" />
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div>
+                    <InputLabel for="date" value="Date" />
 
-                <TextInput
-                    id="date"
-                    type="date"
-                    class="mt-1 block w-full"
-                    v-model="form.date"
-                    required
-                />
+                    <TextInput
+                        id="date"
+                        type="date"
+                        v-model="form.date"
+                        class="mt-1 block w-full"
+                        required
+                    />
 
-                <InputError class="mt-2" :message="form.errors.date" />
+                    <InputError class="mt-2" :message="form.errors.date" />
+                </div>
+
+                <div>
+                    <InputLabel for="location" value="Location" />
+
+                    <TextInput
+                        id="location"
+                        type="text"
+                        class="mt-1 block w-full"
+                        v-model="form.location"
+                        required
+                    />
+
+                    <InputError class="mt-2" :message="form.errors.location" />
+                </div>
             </div>
 
             <div>
@@ -92,5 +108,5 @@ const submit = () => {
                 </PrimaryButton>
             </div>
         </form>
-    </GuestLayout>
+    </AuthenticatedLayout>
 </template>
