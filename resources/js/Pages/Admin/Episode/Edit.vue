fa9c3e8139a4b320eafe83425d16af17556543f5
armazade <armantje2001@gmail.com>
2025-10-22 14:47:36 +0200

    implement style changes in forms

--- resources/js/Pages/Admin/Episode/Edit.vue
+++ resources/js/Pages/Admin/Episode/Edit.vue
@@ -35,95 +35,101 @@ const submit = () => {
     <AuthenticatedLayout>
         <Head title="Edit Episode" />
 
-        <form @submit.prevent="submit" class="max-w-4xl mx-auto space-y-6 mt-10">
-            <div>
-                <InputLabel for="title" value="Episode Title" />
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-neutral-800 shadow-sm sm:rounded-lg">
+                    <form @submit.prevent="submit" class="max-w-4xl mx-auto space-y-6 mt-10">
+                        <div>
+                            <InputLabel for="title" value="Episode Title" />
 
-                <TextInput
-                    id="title"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.title"
-                    required
-                />
+                            <TextInput
+                                id="title"
+                                type="text"
+                                class="mt-1 block w-full"
+                                v-model="form.title"
+                                required
+                            />
 
-                <InputError class="mt-2" :message="form.errors.title" />
-            </div>
+                            <InputError class="mt-2" :message="form.errors.title" />
+                        </div>
 
-            <div>
-                <InputLabel for="description" value="Description" />
+                        <div>
+                            <InputLabel for="description" value="Description" />
 
-                <TextInput
-                    id="description"
-                    v-model="form.description"
-                    class="mt-1 block w-full"
-                ></TextInput>
+                            <TextInput
+                                id="description"
+                                v-model="form.description"
+                                class="mt-1 block w-full"
+                            ></TextInput>
 
-                <InputError class="mt-2" :message="form.errors.description" />
-            </div>
+                            <InputError class="mt-2" :message="form.errors.description" />
+                        </div>
 
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div>
-                    <InputLabel for="date" value="Date" />
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <div>
+                                <InputLabel for="date" value="Date" />
 
-                    <TextInput
-                        id="date"
-                        type="date"
-                        v-model="form.date"
-                        class="mt-1 block w-full"
-                        required
-                    />
+                                <TextInput
+                                    id="date"
+                                    type="date"
+                                    v-model="form.date"
+                                    class="mt-1 block w-full"
+                                    required
+                                />
 
-                    <InputError class="mt-2" :message="form.errors.date" />
+                                <InputError class="mt-2" :message="form.errors.date" />
+                            </div>
+
+                            <div>
+                                <InputLabel for="location" value="Location" />
+
+                                <TextInput
+                                    id="location"
+                                    type="text"
+                                    class="mt-1 block w-full"
+                                    v-model="form.location"
+                                    required
+                                />
+
+                                <InputError class="mt-2" :message="form.errors.location" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <InputLabel for="genres" value="Genres" />
+                            <GenreSelector v-model="form.genres" />
+                            <InputError class="mt-2" :message="form.errors.genres" />
+                        </div>
+
+                        <div>
+                            <InputLabel for="image" value="Episode Image"/>
+                            <div v-if="episode.image" class="mt-2 mb-4">
+                                <p class="text-sm text-neutral-200">Current image:</p>
+                                <img :src="episode.image" alt="Current episode image" class="mt-1 max-w-xs rounded"/>
+                            </div>
+                            <input
+                                id="image"
+                                type="file"
+                                accept="image/*"
+                                @change="e => form.image = e.target.files[0]"
+                                class="mt-1 block w-full"
+                            />
+                            <p class="text-sm text-neutral-200 mt-1">Leave empty to keep current image</p>
+                            <InputError :message="form.errors.image" class="mt-2"/>
+                        </div>
+
+                        <div class="flex justify-end">
+                            <PrimaryButton
+                                class="ml-4 mb-5"
+                                :class="{ 'opacity-25': form.processing }"
+                                :disabled="form.processing"
+                            >
+                                Update Episode
+                            </PrimaryButton>
+                        </div>
+                    </form>
                 </div>
-
-                <div>
-                    <InputLabel for="location" value="Location" />
-
-                    <TextInput
-                        id="location"
-                        type="text"
-                        class="mt-1 block w-full"
-                        v-model="form.location"
-                        required
-                    />
-
-                    <InputError class="mt-2" :message="form.errors.location" />
-                </div>
             </div>
-
-            <div>
-                <InputLabel for="genres" value="Genres" />
-                <GenreSelector v-model="form.genres" />
-                <InputError class="mt-2" :message="form.errors.genres" />
-            </div>
-
-            <div>
-                <InputLabel for="image" value="Episode Image"/>
-                <div v-if="episode.image" class="mt-2 mb-4">
-                    <p class="text-sm text-gray-600">Current image:</p>
-                    <img :src="episode.image" alt="Current episode image" class="mt-1 max-w-xs rounded"/>
-                </div>
-                <input
-                    id="image"
-                    type="file"
-                    accept="image/*"
-                    @change="e => form.image = e.target.files[0]"
-                    class="mt-1 block w-full"
-                />
-                <p class="text-sm text-gray-600 mt-1">Leave empty to keep current image</p>
-                <InputError :message="form.errors.image" class="mt-2"/>
-            </div>
-
-            <div class="flex justify-end">
-                <PrimaryButton
-                    class="ml-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Update Episode
-                </PrimaryButton>
-            </div>
-        </form>
+        </div>
     </AuthenticatedLayout>
 </template>
